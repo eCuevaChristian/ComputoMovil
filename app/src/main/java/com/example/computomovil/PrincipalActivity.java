419145d407261d729d1b9c86877a6a76804ecbe4
Christian Eduardo Taipe Cueva <christianecueva@gmail.com>
2024-06-10 18:07:49 -0500

    subir hasta el nuevo activity tictactoe

--- app/src/main/java/com/example/computomovil/PrincipalActivity.java
+++ app/src/main/java/com/example/computomovil/PrincipalActivity.java
@@ -2,6 +2,8 @@ package com.example.computomovil;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -16,6 +18,8 @@ public class PrincipalActivity extends AppCompatActivity {
     private TextView usuarioTextView;
     private TextView passwordTextView;
     private TextView edadTextView;
+    private Button btnCerrarSesion;
+    private Button btnNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,11 +36,34 @@ public class PrincipalActivity extends AppCompatActivity {
         usuarioTextView = findViewById(R.id.TextV_usuario);
         passwordTextView = findViewById(R.id.TextV_password);
         edadTextView = findViewById(R.id.TextV_edad);
+        btnCerrarSesion = findViewById(R.id.sesion_cerrar);
+        btnNext = findViewById(R.id.btn_sgt);
 
         Intent intent = getIntent();
         nombreTextView.setText(intent.getStringExtra("nombre"));
         usuarioTextView.setText(intent.getStringExtra("usuario"));
         passwordTextView.setText(intent.getStringExtra("password"));
         edadTextView.setText(intent.getStringExtra("edad"));
+
+        btnCerrarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cerrarSesion();
+            }
+        });
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentNext = new Intent(PrincipalActivity.this, tictactoe.class);
+                startActivity(intentNext);
+            }
+        });
+    }
+
+    private void cerrarSesion() {
+        Intent intent = new Intent(PrincipalActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
