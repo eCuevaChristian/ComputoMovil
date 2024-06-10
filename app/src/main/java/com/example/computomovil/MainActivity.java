4956337fb5d8b0e5d6a3f59423b054adc5d86bba
Christian Eduardo Taipe Cueva <christianecueva@gmail.com>
2024-06-10 01:43:22 -0500

    Acaba registrar activity_registro.xml

--- app/src/main/java/com/example/computomovil/MainActivity.java
+++ app/src/main/java/com/example/computomovil/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.computomovil;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv_registrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,16 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
+        });
+        tv_registrar=findViewById(R.id.tv_registrar);
+
+        tv_registrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentRegistrar = new Intent(MainActivity.this,RegistroActivity.class);
+                MainActivity.this.startActivity(intentRegistrar);
+            }
         });
     }
 }
