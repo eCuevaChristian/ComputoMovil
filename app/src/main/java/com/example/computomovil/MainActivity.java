142fe01b73984b51488606c2819fc5816b72b1e8
Christian Eduardo Taipe Cueva <christianecueva@gmail.com>
2024-06-11 01:48:30 -0500

    dandole entendibilidad al registro

--- app/src/main/java/com/example/computomovil/MainActivity.java
+++ app/src/main/java/com/example/computomovil/MainActivity.java
@@ -4,11 +4,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,10 +19,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView tv_registrar;
     private EditText usuarioEditText;
     private EditText passwordEditText;
-    private Button btn_ini;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,10 +33,10 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        tv_registrar = findViewById(R.id.tv_registrar);
+        TextView tv_registrar = findViewById(R.id.tv_registrar);
         usuarioEditText = findViewById(R.id.TextV_usu);
         passwordEditText = findViewById(R.id.TextV_pass);
-        btn_ini = findViewById(R.id.btn_ini);
+        Button btn_ini = findViewById(R.id.btn_ini);
 
         usuarioEditText.addTextChangedListener(new TextWatcher() {
             @Override
@@ -57,20 +54,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        tv_registrar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intentRegistrar = new Intent(MainActivity.this, RegistroActivity.class);
-                MainActivity.this.startActivity(intentRegistrar);
-            }
+        tv_registrar.setOnClickListener(v -> {
+            Intent intentRegistrar = new Intent(MainActivity.this, RegistroActivity.class);
+            MainActivity.this.startActivity(intentRegistrar);
         });
 
-        btn_ini.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                login();
-            }
-        });
+        btn_ini.setOnClickListener(v -> login());
     }
 
     private void login() {
